c9927e13c7c2954591662d2ea32e03b2945a9ac2
Carsten Buchberger <c.buchberger@witcom.de>
2018-11-09 07:52:29 +0000

    Init

---
+++ src/main/java/de/witcom/bpm/EventListenerApplication.java
@@ -0,0 +1,7 @@
+package de.witcom.bpm;
+
+import org.camunda.bpm.application.imp.ServletProcessApplication;
+
+@ProcessApplication("CDI Event Listener App")
+public class EventListenerApplication {
+}
